84ccf92b5d0cbf734b2d40b5f654fb748d5e0cb9
Oliver Heward <oliverhewardbie@gmail.com>
2019-01-29 13:22:03 +0000

    initail commit

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+const gulp = require("gulp"),
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify-es").default,
+    browserSync = require("browser-sync").create(),
+    eslint = require("gulp-eslint"),
+    prettyError = require("gulp-prettyerror"),
+    sass = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    cssnano = require("gulp-cssnano");
+
+
+gulp.task("sass", function (done) {
+    gulp
+        .src("./sass/style.scss", {sourcemaps: true})
+        .pipe(prettyError())
+        .pipe(sass())
+        .pipe(
+            autoprefixer({
+                browsers: ["last 2 versions"]
+            })
+        )
+        .pipe(gulp.dest("./build/css"))
+        .pipe(cssnano())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("./build/css"))
+        .on("end", done);
+});
+
+gulp.task("eslint", function (done) {
+    return gulp
+        .src(["./js/*.js"])
+        // Also need to use it here...
+        .pipe(prettyError())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+        .on("end", done);
+});
+
+gulp.task("scripts", gulp.series("eslint", function () {
+    return gulp
+        .src("./js/*.js")
+        .pipe(uglify())
+        .pipe(
+            rename({
+                extname: ".min.js"
+            })
+        )
+        .pipe(gulp.dest("./build/js"));
+}));
+
+gulp.task("html", function (done) {
+    gulp
+        .src("./*.html")
+        .pipe(prettyError())
+        .pipe(gulp.dest("./build"))
+        .on("end", done);
+});
+
+gulp.task("images", function (done) {
+    gulp
+        .src("./assets/images/*")
+        .pipe(prettyError())
+        .pipe(gulp.dest("./build/images"))
+        .on("end", done);
+});
+
+gulp.task("fonts", function (done) {
+    gulp
+        .src("./assets/fonts/*")
+        .pipe(prettyError())
+        .pipe(gulp.dest("./build/fonts"))
+        .on("end", done);
+});
+
+// Set-up BrowserSync and watch
+
+gulp.task("browser-sync", function () {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+
+    gulp
+        .watch(["build/index.html", "build/css/*.css", "build/js/*.js", "build/images/*", "build/fonts/*"])
+        .on("change", browserSync.reload);
+});
+
+gulp.task("watch", function () {
+    gulp.watch("js/*.js", gulp.series("scripts"));
+    gulp.watch("sass/**/*.scss", gulp.series("sass"));
+    gulp.watch("*.html", gulp.series("html"));
+    gulp.watch("./assets/images/*", gulp.series("images"));
+    gulp.watch("./assets/fonts/*", gulp.series("fonts"));
+});
+
+
+gulp.task("build", gulp.parallel("html", "scripts", "sass", "images", "fonts"));
+gulp.task("default", gulp.series("build", gulp.parallel("browser-sync", "watch")));
---
+++ js/api.js
@@ -0,0 +1,68 @@
+$(function() {
+
+    // var newsImage = " ";
+    // var	newsContent = " ";
+    var $results = $('.flexbox-news');
+
+ 	$('.flexbox-news').hide();
+    $('#news-select').on('change', function() {
+        event.preventDefault();
+        selected = $(this).val();
+        $('.flexbox-news').show();
+
+        // $('.holder').css({'visibility': 'visible'});
+        // $('p').css({'visibility': 'visible'});
+        // console.log(selected);
+
+        $.ajax({
+                method: 'GET',
+                url: 'https://api.nytimes.com/svc/topstories/v2/' + selected + '.json?api-key=iZg3OhUhA3W7q0e6XOYMwQc0Cxo4zaHD',
+                dataType: 'json'
+            })
+
+
+            .done(function(data) {
+                const newsContent = data.results;
+                var myIterater = 1;
+                $.each(newsContent, function(key, value) {
+                	$('#content-holder-' + myIterater + ' p').html(value.abstract);
+                    // console.log(value.abstract);
+                    $.each(value.multimedia, function(k, v) {
+                        if (v.format == 'superJumbo') {
+                        	// $("#place-holder").append('<div class="news-item"><p>Title</p><img src="v.url"><span>v.bstract.</span></div>')
+                        	$('#content-holder-' + myIterater).css({"background": "url("+ v.url +") center", "background-size": "cover"});
+                        //     console.log('hello i logged, ' + v.url);
+                        }
+                    })
+                    myIterater++;
+                    // newsImage += '<img src="' + value.url + ' " />';
+                    // newsItems += '<p>' + value.abstract + '</p>';
+                })
+            })
+
+
+        //    $results
+        //    	.empty()
+        //    	.append(newsImage)
+        //    	.fadeIn(1000);
+        //    })
+
+        // .fail(function() {
+        //    $('').append('Sorry there was an error.');
+        // })
+
+    })
+});
+
+
+
+
+// $('.selection').on('change', function() {
+// 	event.preventDefault();
+
+// 	const selected = $(this).val();
+// 		if (selected !== '') {
+// 	console.log('The value you picked is: ' + selected);
+//     }
+// })
+// })
